5e8d00019c87d4af7095c7d594a5fb9e051dc6ec
Minh Nguyen <lambandwoof@gmail.com>
2022-11-15 13:22:55 -0600

    finished api/user.js

--- api/users.js
+++ api/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createUser, getUser, getUserByUsername } = require("../db")
+const { createUser, getUser, getUserByUsername, getPublicRoutinesByUser } = require("../db")
 const jwt = require("jsonwebtoken");
 const { requireUser } = require('./utils')
 const { JWT_SECRET } = process.env;
@@ -9,7 +9,7 @@ const { JWT_SECRET } = process.env;
 // POST /api/users/login
 router.post(`/login`, async (req, res, next) => {
     const { username, password } = req.body;
-
+    
     if (!username || !password) {
         next({
             name: "MissingCredentialsError",
@@ -18,16 +18,17 @@ router.post(`/login`, async (req, res, next) => {
     }
     try {
         const user = await getUserByUsername(username);
-
+        
         if (user && user.password == password) {
             const token = jwt.sign({username, id:user.id}, JWT_SECRET)
-            res.send({ message:"You're logged in!", token});
+            res.send({ message:"you're logged in!", token, user});
         } else {
             next({
                 name: `IncorrectCredentialsError`,
                 message: `Username or password is incorrect`
             });
         }
+        
     } catch (error) {
         next(error);
     }
@@ -36,13 +37,14 @@ router.post(`/login`, async (req, res, next) => {
 // POST /api/users/register
 router.post(`/register`, async (req, res, next) => {
     const { username, password} = req.body;
-try {
-if(password.length < 8){
-    next({
-        name: `PasswordLengthError`,
-        message: `Password Too Short!`
-    });
-}
+
+    try {
+        if(password.length < 8){
+            next({
+                name: `PasswordLengthError`,
+                message: `Password Too Short!`
+            });
+        }
 
     const _user = await getUserByUsername(username);
 
@@ -79,5 +81,14 @@ router.get("/me", requireUser, async (req,res,next) => {
     }
 })
 // GET /api/users/:username/routines
+router.get("/:username/routines", requireUser, async (req, res, next) =>{
+    try {
+        const user = await getUserByUsername(req.params.username);
+        const routines = await getPublicRoutinesByUser(user)
+        res.send(routines)
+    } catch (error) {
+        next(error);
+    }
+})
 
 module.exports = router;
